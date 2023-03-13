782130dd4b3c6de155860b54732c7837fefde798
Lene <lenecarpertunnel@gmail.com>
2023-03-13 11:56:56 +0100

    Initial commit

---
+++ inc/functions.php
@@ -0,0 +1,119 @@
+<?php
+    // Start a PHP session to store local variables
+    session_start();
+
+    $last_guess = user_guess();
+
+    // Define the minimum and maximum numbers to guess between
+    define("RAND_MIN", 1);
+    define("RAND_MAX", 100);
+
+
+    // // Check whether the guessed number is a numeric value
+    // function valid_last_guess()
+    // {
+    //     if (is_numeric($last_guess))
+    //     {
+    //         $last_guess = 
+    //     }
+    // }
+
+    // Check the user's submitted guess
+    function guess_submitted()
+    {
+        return isset($_POST['guess']);
+    }
+    
+    // Check if the user's guess is the same as the secret number
+    function correct_guess()
+    {
+        return user_guess() == secret_number();
+    }
+    
+    // Check if the user guessed too high
+    function guessed_high()
+    {
+        return user_guess() > secret_number();
+    }
+    
+    // Check if the user guessed too low
+    function guessed_low()
+    {
+        return user_guess() < secret_number();
+    }
+
+    // Check if the user requested a reset through the reset button
+    function request_reset()
+    {
+        return isset($_POST['reset']);
+    }
+
+    // Make sure all keys and conditions are set to prevent errors, otherwise return false
+    function conditional_fetch($hash, $key)
+    {
+        if (isset($hash[$key])) {
+            return $hash[$key];
+        } else {
+            return false;
+        }
+    }
+
+    // Check the user's guess
+    function user_guess()
+    {
+        return conditional_fetch($_POST, 'user_guess');
+    }
+    
+    // Store the created random number in $_SESSION
+    function secret_number()
+    {
+        return conditional_fetch($_SESSION, 'secret_number');
+    }
+    
+    // Check if the secret number is set, return it as unset if not
+    function number_unset()
+    {
+        return !isset($_SESSION['secret_number']);
+    }
+    
+    // Create a random number between the set MIN/MAX values
+    function reset_secret_number()
+    {
+        $_SESSION['secret_number'] = rand(RAND_MIN, RAND_MAX);
+    }
+    
+    // Reset the user's guess count to 0
+    function reset_guess_count()
+    {
+        $_SESSION['guess_count'] = 0;
+    }
+    
+    // Increment the user's guess count after each confirmed guess
+    function increment_guess_count()
+    {
+        $_SESSION['guess_count']++;
+    }
+    
+    // Check the user's total amount of guesses and save it in $_SESSION
+    function guess_count()
+    {
+        return conditional_fetch($_SESSION, 'guess_count'); 
+    }
+
+    function validate_number()
+    {
+        $number = $_POST['user_guess'];
+
+        if (isset($_POST['guess']))
+        {
+            if(is_numeric($number))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+?>
---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+    // Require the functions file to import external functions
+    require('inc/functions.php');
+    
+    // Set a local variable to display a message when the page loads
+    // In this case it's an explanation which displays the MIN/MAX guess values
+    $message = "Guess a number between " . RAND_MIN . " and " . RAND_MAX . ". I will tell you whether your guess was too high, too low or correct.";
+
+    // Store the secret number in a variable which is created when the page loads
+    $correct_guess = correct_guess();
+
+    // Check if the user has submitted a guess, increment the guess counter
+    if (guess_submitted())
+    {
+        increment_guess_count();
+
+        // Display different messages to the user after guessing depending on their answer
+        if ($correct_guess && validate_number() == true)
+        {
+            $message = "You got it! It took you " . guess_count() . " attempts. Guess again?";
+        } else if (guessed_low() && validate_number() == true)
+        {
+            $message = "Sorry, guess again but higher.";
+        } else if (guessed_high() && validate_number() == true)
+        {
+            $message = "Sorry, guess again but lower.";
+        // Make sure only numerical values are guessed
+        } else if (guessed_high() or guessed_low() or $correct_guess && validate_number() == false)
+        {
+            $message = "Please only enter numerical values.";
+        }
+    }
+
+    // Check if the secret number is set, the user has requested a reset or the user guessed the number correctly
+    if (number_unset() || request_reset() || $correct_guess)
+    {
+        reset_secret_number();
+        reset_guess_count();
+    }
+        
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Guess The Number</title>
+    <link rel="stylesheet" href="style/style.css" />
+</head>
+<body>
+    <h1 id="index-header">GUESS THE NUMBER</h1>
+    <!-- Display a local message depending on the state of the game, saved in $_SESSION -->
+    <p class="user-message"><?= $message ?></p>
+
+    <!-- The form to guess the number in -->
+    <div id="form-container">
+        <form method="POST">
+            <label for="user_guess" class="user-message">YOUR GUESS</label><br>
+            <input id="user_guess" name="user_guess" class="guess-form" maxlength="3"><br><br>
+            <input type="submit" name="guess" value="Guess" class="btn-guess">
+            <input type="submit" name="reset" value="Reset" class="btn-reset">
+        </form><br>
+    </div>
+    <div id="guess-container"><h1 class="previous-guess">Previous guess: <?= $last_guess ?><br><br>Secret number: <?= secret_number() ?></h1></div>
+</body>
+</html>
